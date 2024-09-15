3bb82a729807ea7d0e0ad04c4eab741655a4e0ff
Arthur Vargas <ahvargas92@gmail.com>
2024-09-15 12:31:04 -0500

    define shell context processor

--- app/timelight.py
+++ app/timelight.py
@@ -3,4 +3,12 @@ NOTE:
     The Flask application instance called "app" is imported from the package, also called "app"
 """
 
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Activity
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"sa": sa, "so": so, "db": db, "User": User, "Activity": Activity}
